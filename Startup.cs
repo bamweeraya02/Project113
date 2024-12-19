46efae514e50503dcb31e773e5b6e5a419f47ca8
bamweeraya02 <164457372+bamweeraya02@users.noreply.github.com>
2024-12-19 18:56:23 +0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookDemo6.Startup))]
+namespace BookDemo6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
